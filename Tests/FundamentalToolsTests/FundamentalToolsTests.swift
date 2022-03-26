97c3e54e47632aaab4ca668264516e63bc6845d2
roman.guseynov <roman.guseynov@lamoda.ru>
2022-03-26 16:02:55 +0300

    Upd Array

--- Tests/FundamentalToolsTests/FundamentalToolsTests.swift
+++ Tests/FundamentalToolsTests/FundamentalToolsTests.swift
@@ -2,10 +2,46 @@ import XCTest
 @testable import FundamentalTools
 
 final class FundamentalToolsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FundamentalTools().text, "Hello, World!")
+
+    func testArrayCreation() throws {
+        let arr: [Int] = []
+        let arr2 = [Int]()
+        let arr3 = Array<Int>()
+        XCTAssertEqual(arr, [])
+        XCTAssertEqual(arr2, [])
+        XCTAssertEqual(arr3, [])
     }
+
+    func testSetCreation() throws {
+        let setFirst: Set<Int> = []
+        let setSecond = Set<Int>()
+        XCTAssertEqual(setFirst, Set<Int>())
+        XCTAssertEqual(setSecond, Set<Int>())
+    }
+
+    func testDictionaryCreation() throws {
+        let dictionaryFirst: Dictionary<String, Int> = [:]
+        let dictionarySecond = Dictionary<String, Int>()
+        let dictionaryThird = [String: Int]()
+        XCTAssertEqual(dictionaryFirst, [:])
+        XCTAssertEqual(dictionarySecond, [:])
+        XCTAssertEqual(dictionaryThird, [:])
+        XCTAssertEqual(dictionaryFirst, [:])
+    }
+
+    func testIteration() {
+        let intArray = [1,2,3,4,5,6]
+        let testArray = [0,0,0,0,0]
+
+        for (index, value) in intArray.enumerated() {
+            print(value)
+        }
+
+        for i in testArray.indices {
+            print(i)
+        }
+
+    }
+
+
 }
